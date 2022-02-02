3618f024bab7c89a3585ba7baba37bfa1d034283
Hualdo <hucastillo@gmail.com>
2022-02-02 09:50:27 -0500

    Final Mobile and Desktop version, Enjoy ...

--- blogr.js
+++ blogr.js
@@ -1,19 +1,29 @@
 document.getElementById('product').addEventListener('click', displaySub)
 document.getElementById('company').addEventListener('click', displaySub1)
 document.getElementById('connect').addEventListener('click', displaySub2)
+document.getElementById('menuRight').addEventListener('click',displaySubMobile)
 
 function displaySub(){
     document.getElementById('content1').classList.toggle('contentVisible')
     document.getElementById('imag').classList.toggle('rotate')
+    document.getElementById('imag3').classList.toggle('rotate')
 }
 
 function displaySub1(){
     document.getElementById('content2').classList.toggle('contentVisible')
     document.getElementById('imag1').classList.toggle('rotate')
+    document.getElementById('imag4').classList.toggle('rotate')
 }
 
 function displaySub2(){
     document.getElementById('content3').classList.toggle('contentVisible')
     document.getElementById('imag2').classList.toggle('rotate')
+    document.getElementById('imag5').classList.toggle('rotate')
+}
+
+function displaySubMobile(){
+    document.getElementById('hamburger').classList.toggle('hamburgerVisible')
+    document.getElementById('close').classList.toggle('visibleClose')
+    document.getElementById('left').classList.toggle('leftVisible')
 }
 
